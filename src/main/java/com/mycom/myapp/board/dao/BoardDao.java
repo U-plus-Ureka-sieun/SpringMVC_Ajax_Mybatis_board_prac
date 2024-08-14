2350e8810bb996e57ce76795bb800a4053945ccc
Sieun53 <tldms1176@hanmail.net>
2024-08-14 09:18:12 +0900

    board

---
+++ src/main/java/com/mycom/myapp/board/dao/BoardDao.java
@@ -0,0 +1,15 @@
+package com.mycom.myapp.board.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.mycom.myapp.board.dto.BoardDto;
+import com.mycom.myapp.board.dto.BoardParamDto;
+
+@Mapper
+public interface BoardDao {
+	List<BoardDto> listBoard(BoardParamDto boardParamDto);
+	
+	
+}
